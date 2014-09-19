386fef63807218171cb072027f20caea1d0c1423
Stefanos Stavrotheodoros <sstayrotheodoros@hotmail.com>
2014-09-20 02:13:17 +0300

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -6,15 +6,18 @@
 makeCacheMatrix <- function(x = matrix()) {
   
   inv <- NULL
+  
   set <- function(y) {   
     x <<- y
     inv <- NULL
-  }
-  
+  }  
   get <- function() x
+  
   setinverse <- function(inverse) inv <<- inverse
   getinverse <- function() inv
-  list(set = set, get = get, setmean = setmean,getmean, getmean)
+  
+  list(set = set, get = get, 
+       setinverse = setinverse, getinverse = getinverse)
 
 }
 
